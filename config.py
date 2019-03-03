ea4b266fc3432a7959fd3eb6b503da7eeeb54353
MrVeshij <35579329+MrVeshij@users.noreply.github.com>
2019-03-03 16:40:57 +0300

    heroke deployment changes v0.4.7

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ load_dotenv('C:\\Users\\veshi\\Desktop\\Projects\\Site_Moneta\\keys.env')
 
 class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')# or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
