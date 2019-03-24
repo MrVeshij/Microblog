fc51ea570a2404ac710b5eda9c8c6114a1d3ef60
MrVeshij <35579329+MrVeshij@users.noreply.github.com>
2019-03-24 21:40:15 +0300

    change first page

--- app/main/forms.py
+++ app/main/forms.py
@@ -23,6 +23,6 @@ class EditProfileForm(FlaskForm):
 
 class PostForm(FlaskForm):
     post = TextAreaField('Скажите что-нибудь', validators=[
-        DataRequired(), Length(min=1, max=140)])
+        DataRequired(), Length(min=1)])
     submit = SubmitField('Отправить')
 
